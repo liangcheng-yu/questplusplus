373a8c7516f87da7da13fbea9c29f3a4e70f8898
liangcheng-yu <liangcheng.yu46@gmail.com>
2019-05-05 19:14:28 +0800

    Update feature extraction scripts

--- extract_feature_my.py
+++ extract_feature_my.py
@@ -1,6 +1,7 @@
 # Run this to obtain the extracted datas for our feature sets
 import os
 
+# Toggle extra features
 extraFeatureIndices = [
     1007,
     1017,
@@ -14,6 +15,9 @@ extraFeatureIndices = [
     1083,
     1084,
 ]
+# Assume prior offline extraction run
+withSourceDep = True  # Index 10000
+withTargetDep = True  # Index 10001
 
 QUEST_DIR = "/Users/liangchengyu/questplusplus/"
 print("Current dir:")
@@ -26,6 +30,29 @@ for index in extraFeatureIndices:
     os.system("java -cp QuEst++.jar shef.mt.DocLevelFeatureExtractor -case no -lang chinese english -input input/source.doc-level.ch input/target.doc-level.en -config config/"+str(index)+".doc-level.properties")
     print("Copy the extracted feature output...")
     os.system("cp "+QUEST_DIR+"output/mytest/output.txt "+QUEST_DIR+"learning/data/features/"+str(index)+"_raw/feature")
+
+# Merge the dependency related features with the baseline features repectively
+os.system('ant -v && java -cp QuEst++.jar shef.mt.DocLevelFeatureExtractor -case no -lang chinese english -input input/source.doc-level.ch input/target.doc-level.en -config config/baseline.doc-level.properties')
+if withSourceDep:
+    with open(QUEST_DIR+"learning/data/features/10000_raw/feature", 'w', encoding="utf-8") as outfile:
+        with open(QUEST_DIR+"output/mytest/output.txt", 'r', encoding='utf-8') as infileBaseline:
+            with open(QUEST_DIR+"sourceDepDistance.txt", 'r', encoding='utf-8') as infileSource:
+                linesBaseline = infileBaseline.readlines()
+                linesSource = infileSource.readlines()
+                if len(linesBaseline)==len(linesSource):
+                    for idx in range(len(linesBaseline)):
+                        lineW = linesBaseline[idx].strip('\n')+"\t"+linesSource[idx]
+                        outfile.write(lineW)
+if withTargetDep:                        
+    with open(QUEST_DIR+"learning/data/features/10001_raw/feature", 'w', encoding="utf-8") as outfile:
+        with open(QUEST_DIR+"output/mytest/output.txt", 'r', encoding='utf-8') as infileBaseline:
+            with open(QUEST_DIR+"targetDepDistance.txt", 'r', encoding='utf-8') as infileTarget:
+                linesBaseline = infileBaseline.readlines()
+                linesTarget = infileTarget.readlines()
+                if len(linesBaseline)==len(linesTarget):
+                    for idx in range(len(linesBaseline)):
+                        lineW = linesBaseline[idx].strip('\n')+"\t"+linesTarget[idx]
+                        outfile.write(lineW)
 
 # Run the combined feature set
 os.system("java -cp QuEst++.jar shef.mt.DocLevelFeatureExtractor -case no -lang chinese english -input input/source.doc-level.ch input/target.doc-level.en -config config/my.doc-level.properties")
